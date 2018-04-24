5729f0ba98a950372f3f32fc9ea412ced6297c41
David Alber <alber.david@gmail.com>
2018-04-23 21:29:14 -0700

    Adding mechanism for inserting tag descriptions

--- make-tag-files.sh
+++ make-tag-files.sh
@@ -5,5 +5,9 @@ tags=`find _posts -type f | xargs grep "^tags:" | cut -d[ -f2 | cut -d] -f1 | tr
 IFS=$'\n'
 for tag in $tags; do
     echo Making tag file for \"$tag\"
-    printf -- "---\nlayout: tagpage\ntitle: \"Tag: $tag\"\ntag: $tag\n---\n" > tag/"$tag.md";
+    printf -- "---\nlayout: tagpage\ntitle: \"Tag: $tag\"\ntag: $tag\n" > tag/"$tag.md";
+    if [ -f _tag_descriptions/$tag ]; then
+	printf -- "description: \"`cat _tag_descriptions/$tag`\"\n" >> tag/"$tag.md";
+    fi
+    printf -- "---\n" >> tag/"$tag.md";
 done
